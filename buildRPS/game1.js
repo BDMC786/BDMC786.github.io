5a9593e10d2496b94319c56fb6fdb6a89a494b96
BDMC786 <44556616+BDMC786@users.noreply.github.com>
2019-05-31 12:38:06 -0400

    Update game1.js

--- buildRPS/game1.js
+++ buildRPS/game1.js
@@ -1,5 +1,6 @@
 // Select the submit button
-
+// Making a more intelligent game
+console.log("Start Here");
 var w = 0
 var l = 0
 var d = 0
@@ -18,10 +19,48 @@ submit.on("click", function() {
   // Get the value property of the input element
   var PlayerInput = inputElement.property("value");
 
+//Convert different inputs to "Word" form eg r = Rock
+
+  console.log(PlayerInput);
+
+  console.log("Test");
+
+
+  if (PlayerInput == "r"){
+    PlayerInput = "Rock";}
+
+  else if (PlayerInput == "rock"){
+    PlayerInput = "Rock";}
+
+  else if (PlayerInput == "R"){
+    PlayerInput = "Rock";}
+
+
+
+  if (PlayerInput == "p"){
+    PlayerInput = "Paper";}
+
+  else if (PlayerInput == "paper"){
+    PlayerInput = "Paper";}
+
+  else if (PlayerInput == "P"){
+    PlayerInput = "Paper";}
+
+
+  if (PlayerInput == "s"){
+    PlayerInput = "Scissors";}
+
+  else if (PlayerInput == "scissors"){
+    PlayerInput = "Scissors";}
+
+  else if (PlayerInput == "S"){
+    PlayerInput = "Scissors";}
+
+
   console.log(PlayerInput);
 
 
-  var choice = ["r", "p", "s"];
+  var choice = ["Rock", "Paper", "Scissors"];
   var win = "Congratulations! You win!";
   var lose = "You lose. Better luck next time!";
   var tie = "It's a draw!";
@@ -38,26 +77,26 @@ submit.on("click", function() {
 
 
   //logic for gameplay
-  if (PlayerInput == "r") {
-    if (computerChoice == "r") {result = tie; d += 1;}
-    else if (computerChoice == "p") {result = lose; l += 1;}
-    else if (computerChoice == "s") {result = win; w += 1;}
+  if (PlayerInput == "Rock") {
+    if (computerChoice == "Rock") {result = tie; d += 1;}
+    else if (computerChoice == "Paper") {result = lose; l += 1;}
+    else if (computerChoice == "Scissors") {result = win; w += 1;}
     }
-  else if (PlayerInput == "p") {
-    if (computerChoice == "r") {result = win; w += 1;}
-    else if (computerChoice == "p") {result = tie; d += 1;}
-    else if (computerChoice == "s") {result = lose; l += 1;}
+  else if (PlayerInput == "Paper") {
+    if (computerChoice == "Rock") {result = win; w += 1;}
+    else if (computerChoice == "Paper") {result = tie; d += 1;}
+    else if (computerChoice == "Scissors") {result = lose; l += 1;}
     }
-  else if (PlayerInput == "s") {
-    if (computerChoice == "r") {result = lose; l += 1;}
-    else if (computerChoice == "p") {result = win; w += 1;}
-    else if (computerChoice == "s") {result = tie; d += 1;}
+  else if (PlayerInput == "Scissors") {
+    if (computerChoice == "Rock") {result = lose; l += 1;}
+    else if (computerChoice == "Paper") {result = win; w += 1;}
+    else if (computerChoice == "Scissors") {result = tie; d += 1;}
     }
 
 
 
 
-  else {result = "I did not understand that! Please choose r, p, or s."}
+  else {result = "I did not understand that! Please choose Rock, Paper, or Scissors."}
   console.log(w);
   console.log(l);
   console.log(d);
@@ -80,7 +119,8 @@ submit.on("click", function() {
   d3.select("#cChoice").text(`The Computer chose: ${computerChoice}`);
   d3.select("#result").text(result);
   d3.select("#counter").text(`You have won ${w} games, lost ${l} games, and had ${d} draws.`);
-  d3.select("#rematch").text("Play Again?");
+  d3.select("#rematch").text("Play Again?")
+  console.log("End Here");;
 
 });
 
