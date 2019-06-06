a228a7521cecaaf450f66bf31d6b82c9eca6cfa5
BDMC786 <44556616+BDMC786@users.noreply.github.com>
2019-06-06 17:08:34 -0400

    Add files via upload

---
+++ Loan/loans.js
@@ -0,0 +1,32 @@
+var submit = d3.select("#submit");
+
+//id="submit"
+
+submit.on("click", function() {
+//prevent page from refreshing
+  d3.event.preventDefault();
+
+//capture 3 input fields
+  var loan = inputElement.property("loan");
+  var rate = inputElement.property("rate");
+  var time = inputElement.property("time");
+
+//Perform Calculations
+  var i;
+  rate = (1 + (rate/100);
+
+//variable for cumulative interest etc
+  var tinterest = 0;
+  var balance = 0;
+  var minterest = 0; //interest monthly
+  
+
+
+//LOOP
+  for (i = 0; i < time; i++){
+//loop and appending table below until "}"
+    //calculate month's interest- minterest
+    
+  }
+
+});
