cce7d42e7fe966477fee5ee4ff7cb892013548de
BDMC786 <44556616+BDMC786@users.noreply.github.com>
2019-06-10 17:19:40 -0400

    Update loans.js

--- Loan/loans.js
+++ Loan/loans.js
@@ -1,35 +1,118 @@
+console.log("Start");
+
 var submit = d3.select("#submit");
+console.log(submit);
 
-//id="submit"
-
-submit.on("click", function() {
-//prevent page from refreshing
+submit.on("click", function(){
   d3.event.preventDefault();
+  console.log("Click");
 
-//capture 3 input fields
-  var loan = inputElement.property("loan");
-  var rate = inputElement.property("rate");
-  var time = inputElement.property("time");
+//capture inputs
+  var PV = d3.select("#loan");
+  console.log(PV.property("value"));
+  loan = PV.property("value");
+  var rate = d3.select("#rate");
+  console.log(rate.property("value"));
+  r = rate.property("value");
+  var time = d3.select("#time");
+  console.log(time.property("value"));
+  months = time.property("value");
 
-//Perform Calculations
+//calculate payment
+  r = r/1200
+  console.log(r);
+  var P = (r*loan)/(1-(1+r)**((0-1) * months));
+  console.log(P);
+  Pr = Math.round(P);
+  console.log(Pr);
+
+//Append Payment calculation
+  d3.select("#Payment").text(`Your monthly payment is: $${Pr}`);
+
+//Add table header to page
+  //Define column names
+  var columns = [{head: "Date"},
+    {head: "Interest"},
+    {head: "Cumulative Interest"},
+    {head: "Principal"},
+    {head: "Total Principal Paid"},
+    {head: "Remaining Balance"}];
+
+  //append <head> to "#target"
+  var target = d3.select("#target");
+  var row = target.append("table");
+
+  //Add <thead> by selecting <table>
+  var table = d3.select("table");
+  var row = table.append("thead");
+
+  //Add <tr> by selecting <thead> and add column array elements to header
+  var thead = d3.select("thead");
+  var row = thead.append("tr");
+    columns.forEach((column) => {
+    Object.entries(column).forEach(([key, value]) => {
+      var cell = thead.append("th");
+      cell.text(value);
+    });
+  });
+
+//Add table body
+  //Append body
+  var table = d3.select("table");
+  var row = table.append("tbody");
+  //Make an array to hold values
+  var data = [];
+
+  //Make an array of the column headers
+  var headers = ["Date", "Interest", "Cumulative Interest", "Principal", "Total Principal Paid", "Remaining Balance"];
+
+  //Perform Calculations
+  //define variables
+  var interest = 0;
+  var c_int = 0;
+  var prin = 0;
+  var c_prin = 0;
+  var bal = loan;
+
+  //establish loop
+  console.log("LOOP");
+  months = parseInt(months);
+  console.log(months);
   var i;
-  rate = (1 + (rate/100);
 
-//variable for cumulative interest etc
-  var tinterest = 0;
-  var balance = 0;
-  var minterest = 0; //interest monthly
-  
-//before the loop to calculate each month and append table row, append table header to target
-          
-          
-          
+  for (i = 0; i < months; i++) {
+  //calculate interest
+  interest = r * bal;
+  interest = Math.round(interest);
+  console.log(interest);
+  //interest cumulative
+  c_int = c_int + interest;
+  console.log(c_int);
+  //calculate prin
+  prin = Pr - interest
+  console.log(`prin = ${prin}`);
+  //calculate cumulative principle
+  c_prin = c_prin + prin
+  console.log("c_prin");
+  //calculate new balance
+  bal = bal - prin;
+  console.log(`remaining balance = ${bal}`);
 
-//LOOP
-  for (i = 0; i < time; i++){
-//loop and appending table below until "}"
-    //calculate month's interest- minterest
-    
+  console.log("END");
+  console.log("Append Body");
+
   }
+//end of loop
+
+
+
+
+
+
+
+
+
+
+
 
 });
