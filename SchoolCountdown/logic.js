145fd6febd500886e405a583ea0a8c0d38b0befc
BDMC786 <44556616+BDMC786@users.noreply.github.com>
2019-06-11 17:10:41 -0400

    Update logic.js

--- SchoolCountdown/logic.js
+++ SchoolCountdown/logic.js
@@ -19,7 +19,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("demo").innerHTML = "Jessica's school year ends in:" + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
