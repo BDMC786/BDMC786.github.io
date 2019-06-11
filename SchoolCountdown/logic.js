ce3654e2cf9025c8b75a83058105723b8a7c3fbc
BDMC786 <44556616+BDMC786@users.noreply.github.com>
2019-06-11 17:07:02 -0400

    Update logic.js

--- SchoolCountdown/logic.js
+++ SchoolCountdown/logic.js
@@ -1,7 +1,7 @@
 console.log("Hello World");
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+var countDownDate = new Date("Jun 24, 2019 13:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
